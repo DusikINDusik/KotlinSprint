4df3daf67d6274745232823da7833b0ee375b363
Eduard.Shakhov <eduard@>
2024-12-13 18:57:08 +0500

    KS-1-2 the solution to lesson 1 task 2

---
+++ src/main/kotlin/Lesson1/lesson1_task2.kt
@@ -0,0 +1,18 @@
+package org.example.Lesson1
+
+fun main (){
+    var numberOfOrder: Int = 75
+    val messageToOrder: String = "Количество товара: "+ numberOfOrder + ". Спасибо за ваш заказ! Будем рады видеть вас снова!"
+
+    println (numberOfOrder)
+    println (messageToOrder)
+
+    var shopEmployees: Int = 2000
+
+    //println (shopEmployees)
+
+    shopEmployees = 1999
+
+    println (shopEmployees)
+
+}
