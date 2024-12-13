c1b105cb0e63b8cbe894319366d86113ac716921
Eduard.Shakhov <eduard@>
2024-12-13 20:06:55 +0500

    KS-1-4 the solution to lesson 1 task 4

---
+++ src/main/kotlin/Lesson1/lesson1-task4.kt
@@ -0,0 +1,17 @@
+package org.example.Lesson1
+
+fun main (){
+    val distance: Long = 4086860000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Int = 6480
+    val partOfTheYear: Double = 2.0547945205479453E-4
+    val apogeeOfTheOrbit: Int = 327000
+
+    println(distance)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfTheYear)
+    println(apogeeOfTheOrbit)
+}
