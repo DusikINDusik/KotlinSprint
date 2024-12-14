4ebca84c927d93e4677450705dd0cf507815b896
Eduard.Shakhov <eduard@>
2024-12-14 18:52:16 +0500

    KS-2-1 the solution to lesson 2 task 1

---
+++ src/main/kotlin/Lesson2/lesson2-task1.kt
@@ -0,0 +1,12 @@
+package org.example.Lesson2
+
+fun main() {
+    val scoreOfTheFirstStudent = 3
+    val scoreOfTheSecondStudent = 4
+    val scoreOfTheThirdStudent = 3
+    val scoreOfTheFourthStudent = 5
+
+    val averageScore: Float = (scoreOfTheFirstStudent + scoreOfTheSecondStudent + scoreOfTheThirdStudent + scoreOfTheFourthStudent) / 4f
+
+    println(averageScore)
+}
