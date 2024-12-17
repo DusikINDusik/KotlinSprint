9a31b7edd5649383b1e78f4dd2ffb4b10e3911be
Eduard.Shakhov <eduard@>
2024-12-17 20:36:47 +0500

    KS-2-1 the solution to lesson 2 task 1, corrected

--- src/main/kotlin/Lesson2/lesson2-task1.kt
+++ src/main/kotlin/Lesson2/lesson2-task1.kt
@@ -1,13 +1,14 @@
 package org.example.Lesson2
 
-fun main() {
+fun main () {
     val scoreOfTheFirstStudent = 3
     val scoreOfTheSecondStudent = 4
     val scoreOfTheThirdStudent = 3
     val scoreOfTheFourthStudent = 5
     val numberOfStudents = 4
 
-    val averageScore = ((scoreOfTheFirstStudent.toFloat() + scoreOfTheSecondStudent.toFloat()+ scoreOfTheThirdStudent.toFloat() + scoreOfTheFourthStudent.toFloat()) / numberOfStudents)
+    val averageScore =
+        ((scoreOfTheFirstStudent.toFloat() + scoreOfTheSecondStudent.toFloat() + scoreOfTheThirdStudent.toFloat() + scoreOfTheFourthStudent.toFloat()) / numberOfStudents)
 
     println(averageScore)
 }
