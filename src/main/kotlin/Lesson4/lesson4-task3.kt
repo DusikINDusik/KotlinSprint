3a55c8d6fc93a4ea228e91c43537ee3839204b1a
Eduard.Shakhov <eduard@>
2025-01-20 20:42:44 +0500

    KS-4-3 the solution to lesson 4 task 3 changed

--- src/main/kotlin/Lesson4/lesson4-task3.kt
+++ src/main/kotlin/Lesson4/lesson4-task3.kt
@@ -4,13 +4,16 @@ fun main() {
     var goodWeather: Boolean = true
     var awningIsOpen: Boolean = true
     var airHumidity = 20
-    var season = "зима"
+    var season = "лето"
 
-    var calculationOfConditions = goodWeather && awningIsOpen && airHumidity==GOOD_AIR_HUMIDITY && season==GOOD_SEASON
+    var calculationOfConditions =
+        goodWeather == SUNNY_WEATHER && awningIsOpen == AWNING_IS_OPEN && airHumidity == GOOD_AIR_HUMIDITY && season != GOOD_SEASON
 
     println("Благоприятные ли сейчас условия для роста бобовых?$calculationOfConditions")
 
 }
 
-const val GOOD_AIR_HUMIDITY = 20
-const val GOOD_SEASON = "не зима"
+const val SUNNY_WEATHER: Boolean = true
+const val AWNING_IS_OPEN: Boolean = true
+const val GOOD_AIR_HUMIDITY: Int = 20
+const val GOOD_SEASON: String = "зима"
