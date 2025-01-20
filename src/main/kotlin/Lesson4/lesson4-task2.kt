521da2b3bf2f033b788d7ca6b9d7342afe237d03
Eduard.Shakhov <eduard@>
2025-01-20 15:23:56 +0500

    KS-3-4 the solution to lesson 3 task 4 changed

--- src/main/kotlin/Lesson4/lesson4-task2.kt
+++ src/main/kotlin/Lesson4/lesson4-task2.kt
@@ -9,8 +9,8 @@ fun main() {
     var weight = 50
     var liters = 10
 
-    println ("Груз с весом $weight кг и объемом $liters л соответствует категории \'Average\':${weight>averageMinWeight && weight<=averageMaxWeight && liters<averageLiters}")
+    println ("Груз с весом $weight кг и объемом $liters л соответствует категории \'Average\':${weight>AVERAGE_MIN_WEIGHT && weight<=AVERAGE_MAX_WEIGHT && liters<AVERAGE_LITERS}")
 }
-const val averageMinWeight = 35
-const val averageMaxWeight = 100
-const val averageLiters = 100
+const val AVERAGE_MIN_WEIGHT = 35
+const val AVERAGE_MAX_WEIGHT = 100
+const val AVERAGE_LITERS = 100
