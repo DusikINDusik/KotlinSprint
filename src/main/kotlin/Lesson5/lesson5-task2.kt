f0876335676b881342bbb9d481d33b752863db0b
Eduard.Shakhov <eduard@>
2025-01-21 21:55:16 +0500

    KS-5-2 the solution to lesson 5 task 2

--- src/main/kotlin/Lesson5/lesson5-task2.kt
+++ src/main/kotlin/Lesson5/lesson5-task2.kt
@@ -4,10 +4,11 @@ fun main() {
     println("Введите дату рождения")
 
     var userAsk = readln().toInt()
-    if (CURRENT_YEAR - userAsk >= AGE_MAJORITY){
+    if (CURRENT_YEAR - userAsk >= AGE_MAJORITY) {
         println("Показывать экран со скрытым контентом")
     }
 
 }
+
 const val AGE_MAJORITY = 18
 const val CURRENT_YEAR = 2025
