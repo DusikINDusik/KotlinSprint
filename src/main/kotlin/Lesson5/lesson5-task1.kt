0799b63fa147cc76d1735f97d0402e4d69f3f0ca
Eduard.Shakhov <eduard@>
2025-01-23 21:26:04 +0500

    KS-5-1 the solution to lesson 5 task 1

--- src/main/kotlin/Lesson5/lesson5-task1.kt
+++ src/main/kotlin/Lesson5/lesson5-task1.kt
@@ -1,13 +1,16 @@
 package org.example.Lesson5
 
 fun main() {
-    println("Для доступа решите задачу: 5 + 10")
+    println("Для доступа решите задачу: $FIRST_NUMBER + $SECOND_NUMBER")
 
     var userAsk = readln().toInt()
 
-    if (userAsk == 15) {
+    if (userAsk == FIRST_NUMBER + SECOND_NUMBER) {
         println("Добро пожаловать")
     } else {
         println("Доступ запрещен.")
     }
 }
+
+const val FIRST_NUMBER = 5
+const val SECOND_NUMBER = 10
