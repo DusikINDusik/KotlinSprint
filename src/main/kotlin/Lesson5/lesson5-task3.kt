a7b8de717c71259cb64fb5c94953de31772c23f3
Eduard.Shakhov <eduard@>
2025-01-23 23:07:31 +0500

    KS-5-3 the solution to lesson 5 task 3

--- src/main/kotlin/Lesson5/lesson5-task3.kt
+++ src/main/kotlin/Lesson5/lesson5-task3.kt
@@ -1,23 +1,28 @@
 package org.example.Lesson5
 
+import kotlin.jvm.internal.Ref.FloatRef
+
 fun main() {
     println("Введите первое число")
 
-    var userFirstAsk = readln().toInt()
+    val userFirstAsk = readln().toInt()
 
     println("Введите второе число")
 
-    var userSecondAsk = readln().toInt()
+    val userSecondAsk = readln().toInt()
 
-    if (userFirstAsk == FIRST_WIN_NUMBER && userFirstAsk == SECOND_WIN_NUMBER || userSecondAsk == FIRST_WIN_NUMBER && userSecondAsk == SECOND_WIN_NUMBER) {
+    if (userFirstAsk == FIRST_WIN_NUMBER && userSecondAsk == SECOND_WIN_NUMBER ||
+        userFirstAsk == SECOND_WIN_NUMBER && userSecondAsk == FIRST_WIN_NUMBER
+    ) {
         println("Поздравляем! Вы выиграли главный приз!")
-    } else if (userFirstAsk == FIRST_WIN_NUMBER || userFirstAsk == SECOND_WIN_NUMBER || userSecondAsk == FIRST_WIN_NUMBER || userSecondAsk == SECOND_WIN_NUMBER) {
+    } else if (userFirstAsk == FIRST_WIN_NUMBER || userSecondAsk == SECOND_WIN_NUMBER ||
+        userFirstAsk == SECOND_WIN_NUMBER || userSecondAsk == FIRST_WIN_NUMBER
+    ) {
         println("Вы выиграли утешительный приз!")
     } else {
         println("Неудача.")
     }
-
-
+    println("Выигрышные числа $FIRST_WIN_NUMBER и $SECOND_WIN_NUMBER")
 }
 
 const val FIRST_WIN_NUMBER = 15
