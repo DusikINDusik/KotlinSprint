e0f37b32445af31846ff92453bed02bd61755901
Eduard.Shakhov <eduard@>
2025-02-05 22:09:58 +0500

    KS-1-5 the solution to lesson 1 task 5 changed

--- src/main/kotlin/Lesson1/lesson1-task5.kt
+++ src/main/kotlin/Lesson1/lesson1-task5.kt
@@ -2,7 +2,7 @@ package org.example.Lesson1
 
 fun main() {
     var secondsInSpace = 6480
-    var hourInSpace = 6480 / 3600
+    var hourInSpace = secondsInSpace / SECOND_IN_HOUR
     var minuteInSpase = (secondsInSpace - (hourInSpace * SECOND_IN_HOUR)) / SECOND_IN_MINUTE
     var secondInSpace = minuteInSpase / SECOND_IN_MINUTE
 
