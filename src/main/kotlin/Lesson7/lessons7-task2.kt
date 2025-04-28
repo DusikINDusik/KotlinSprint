5f04e4f20e56847dd71434c5673c7f8b9a3acf59
Eduard.Shakhov <eduard@>
2025-04-28 19:09:06 +0500

    KS-7-2 the solution to lesson 7 task 2 changed

--- src/main/kotlin/Lesson7/lessons7-task2.kt
+++ src/main/kotlin/Lesson7/lessons7-task2.kt
@@ -1,6 +1,20 @@
 package org.example.Lessons7
 
 fun main() {
+    while (true) {
 
+        var newCode = (1000..9999).random()
 
+        println("Ваш код авторизации: $newCode")
+
+        println("Введите код для авторизации")
+
+        var userAnswer = readln().toInt()
+
+        if (newCode == userAnswer) {
+            println("Добро пожаловать")
+            break
+        } else println("Неверный код")
+
+    }
 }
