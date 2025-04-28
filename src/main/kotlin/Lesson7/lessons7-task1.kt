693564c7939d7ae02923a9ccf0838d079caa01fa
Eduard.Shakhov <eduard@>
2025-04-28 16:56:40 +0500

    KS-7-1 the solution to lesson 7 task 1

---
+++ src/main/kotlin/Lesson7/lessons7-task1.kt
@@ -0,0 +1,16 @@
+package org.example.Lessons7
+
+fun main() {
+    var count = 3
+    var password = ""
+
+    for (i in 1..count) {
+
+        var randomNumber = ('0'..'9').random()
+        var randomChar = ('a'..'z').random()
+        password += "$randomChar$randomNumber"
+    }
+
+    println(password)
+
+}
