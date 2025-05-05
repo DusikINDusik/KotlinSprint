1e8278722a69979389f31ff36e1592521b928ca0
Eduard.Shakhov <eduard@>
2025-05-05 20:21:53 +0500

    KS-8-1 the solution to lesson 8 task 1

---
+++ src/main/kotlin/Lesson8/lesson8-task1.kt
@@ -0,0 +1,10 @@
+package org.example.Lesson8
+
+fun main() {
+    val numbersOfView = intArrayOf(200, 568, 446, 3665, 789, 637, 926)
+
+    val total = numbersOfView.sum()
+
+    println("Колличество просмотров за неделю: $total")
+
+}
