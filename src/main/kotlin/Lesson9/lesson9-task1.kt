5d9852909e8a4b6a1eda24b648ea10161f6397b3
Eduard.Shakhov <eduard@>
2025-05-07 21:56:08 +0500

    KS-9-1 the solution to lesson 9 task 1

---
+++ src/main/kotlin/Lesson9/lesson9-task1.kt
@@ -0,0 +1,11 @@
+package org.example.Lesson9
+
+fun main() {
+    var ingridients = mutableListOf("Сахар", "Творог", "Сметана", "Ягоды")
+
+    println("В рецепте есть следующие ингредиенты:")
+    ingridients.forEach{ listIngridients ->
+        listIngridients
+    println(listIngridients)
+    }
+}
